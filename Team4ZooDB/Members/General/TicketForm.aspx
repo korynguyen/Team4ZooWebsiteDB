49d1a2eba156522c69ef8bd1df91df664ad378ae
Eric Garcia <78274657+Egr711@users.noreply.github.com>
2021-04-20 16:00:06 -0500

    Added Ticket Form

---
+++ Team4ZooDB/Members/General/TicketForm.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TicketForm.aspx.cs" Inherits="TicketForm.TicketForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <label style="margin-left: 50px">NEW TICKET FORM. This puts a new ticket into the database.</label>
+        </div>
+        <div style="margin: 10px">
+            <asp:TextBox ID="price" runat="server"/>
+            <label style="margin-left: 10px">Ticket Price: </label>
+        </div>
+        <div style="margin: 10px">
+            <asp:TextBox ID="date" runat="server" placeholder="yyyy-mm-dd"/>
+            <label style="margin-left: 10px">Date:</label>
+        </div>
+        <div style="margin: 10px">
+            <asp:TextBox ID="boothid" runat="server"/>
+            <label style="margin-left: 10px">TicketBooth ID:</label>
+        </div>
+        <div style="margin: 10px">
+            <asp:TextBox ID="email" runat="server"  />
+            <label style="margin-left: 10px">Email:</label>
+        </div>
+        <div style="margin: 10px"> 
+            <asp:Button ID="SubmitButton" runat="server" OnClick="SubmitButton_Click" Text="Submit"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Team4ZooDB/Members/General/TicketForm.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+
+namespace TicketForm
+{
+    public partial class TicketForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        string connection = "Server=team4zoodb.mysql.database.azure.com; Port=3306; Database=zoo; Uid=Team4@team4zoodb; Pwd=4thTeamRocks; SslMode=Preferred;";
+
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            //Specify and connect to the DB
+            using (MySqlConnection sqlConnection = new MySqlConnection(connection))
+            {
+                string sql = "INSERT INTO ticket(Price, Date, `Ticket Booth_BoothID`, Member_Email) VALUES("+ price.Text + ", " + date.Text + ", " + boothid.Text + ", '" + email.Text + "');";
+
+
+                sqlConnection.Open();
+                // Call the stored routine submitemployee (I custom created it inside mySQL) which will insert all the info from the webform. The parameter are in the comment below
+                MySqlCommand sqlCmd = new MySqlCommand(sql, sqlConnection);//(@s_ID, @s_FName, @s_MInitial, @s_LName, @s_StartDate, @s_Addr, @s_Phone, @s_Sex, @s_DOB)
+
+
+                sqlCmd.ExecuteNonQuery(); // execute query
+                ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "Ticket Inserted" + "');", true); // pop up message when it is done submitting
+                sqlConnection.Close();
+            }
+        }
+    }
+}

62cf144348dc5d23997c9d96fa5751f0c230222f
Egr711 <garcia1999eric@gmail.com>
2021-04-25 16:56:10 -0500

    Removed username from EmployeeForm

--- Team4ZooDB/Members/General/EmployeeFormV2.aspx.designer.cs
+++ Team4ZooDB/Members/General/EmployeeFormV2.aspx.designer.cs
@@ -112,15 +112,6 @@ namespace EmployeeFormV2 {
         protected global::System.Web.UI.WebControls.TextBox email;
         
         /// <summary>
-        /// username control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox username;
-        
-        /// <summary>
         /// password control.
         /// </summary>
         /// <remarks>
